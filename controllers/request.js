732c782a5699ec336461191ee8ecebe89bf7415f
ArchiOrange <dinka4.435@gmail.com>
2019-12-31 19:18:52 +0300

    stable parsing data

--- controllers/request.js
+++ controllers/request.js
@@ -1,221 +1,69 @@
  request = require('request');
  const DB = require('./DB')
-var statistic  = {
-  rate:0,
-  difficulty:0,
-  users:0,
-  user_stales:0,
-  fee:0,
-  current_payouts:0,
-  global_stats:0,
-  local_stats:0,
-  peer_addresses:0,
-  pings:0,
-  recent_blocks:0,
-  currency_info:0,
-  local_hash_rate:{
-          last_hour:0,
-          last_day:0,
-          last_week:0,
-          last_month:0,
-          last_year:0
-  },
-  pool_rates:{
-          last_hour:0,
-          last_day:0,
-          last_week:0,
-          last_month:0,
-          last_year:0
-  },
-  miner_hash_rates:{
-          last_hour:0,
-          last_day:0,
-          last_week:0,
-          last_month:0,
-          last_year:0
-  },
-  current_payouts:{
-          last_hour:0,
-          last_day:0,
-          last_week:0,
-          last_month:0,
-          last_year:0
-  },
-}
 
-exports.request = function (req,res) {
-let promise = new Promise(function(resolve, reject) {
-  request('http://sha256.p2p-spb.xyz:9338/rate', (err, response, rate) => {
-  if(err)
-    console.log(err);
-    statistic.rate = rate
-    return rate
-  });
-
-  request('http://sha256.p2p-spb.xyz:9338/difficulty', (err, response, difficulty) => {
-  if(err)
-    console.log(err);
-    statistic.difficulty = difficulty
-    //console.log(response);
-    return difficulty
-  });
-  request('http://sha256.p2p-spb.xyz:9338/users', (err, response, users) => {
-  if(err)
-    console.log(err);
-    statistic.users = users
-    //console.log(response);
-    return users
-  });
-  request('http://sha256.p2p-spb.xyz:9338/user_stales', (err, response, user_stales) => {
-  if(err)
-    console.log(err);
-    statistic.user_stales = user_stales
-    //console.log(response);
-    res.json(statistic)
-    return user_stales
-  });
-  request('http://sha256.p2p-spb.xyz:9338/fee', (err, response, fee) => {
-  if(err)
-    console.log(err);
-    statistic.fee = fee
-    //console.log(response);
-    return fee
-  });
-  request('http://sha256.p2p-spb.xyz:9338/current_payouts', (err, response, current_payouts) => {
-  if(err)
-    console.log(err);
-    statistic.current_payouts = current_payouts
-    //console.log(response);
-    return current_payouts
-  });
-  request('http://sha256.p2p-spb.xyz:9338/global_stats', (err, response, global_stats) => {
-  if(err)
-    console.log(err);
-    statistic.global_stats = global_stats
-    //console.log(response);
-    return global_stats
-  });
-  request('http://sha256.p2p-spb.xyz:9338/local_stats', (err, response, local_stats) => {
-  if(err)
-    console.log(err);
-    statistic.local_stats = local_stats
-    //console.log(response);
-    return local_stats
-  });
-  request('http://sha256.p2p-spb.xyz:9338/peer_addresses', (err, response, peer_addresses) => {
-  if(err)
-    console.log(err);
-    statistic.peer_addresses = peer_addresses
-    //console.log(response);
-    return peer_addresses
-  });
-  request('http://sha256.p2p-spb.xyz:9338/pings', (err, response, pings) => {
-  if(err)
-    console.log(err);
-    statistic.pings = pings
-    //console.log(response);
-    return pings
-  });
-  request('http://sha256.p2p-spb.xyz:9338/recent_blocks', (err, response, recent_blocks) => {
-  if(err)
-    console.log(err);
-    statistic.recent_blocks = recent_blocks
-    //console.log(response);
-    return recent_blocks
-  });
-  request('http://sha256.p2p-spb.xyz:9338/web/currency_info', (err, response, currency_info) => {///////
-  if(err)
-    console.log(err);
-    statistic.currency_info = currency_info
-    //console.log(response);
-    return currency_info
-  });
-  request('http://sha256.p2p-spb.xyz:9338/local_hash_rate/last_hour', (err, response, local_hash_rate_last_hour) => {
-  if(err)
-    console.log(err);
-    statistic.local_hash_rate = local_hash_rate_last_hour
-    //console.log(response);
-    return local_hash_rate_last_hour
-  });
-  request('http://sha256.p2p-spb.xyz:9338/web/graph_data/pool_rates/last_hour', (err, response, pool_rates_last_hour) => {
-  if(err)
-    console.log(err);
-    statistic.pool_rates_last_hour = pool_rates_last_hour
-    //console.log(response);
-    return pool_rates_last_hour
-  });
-  request('http://sha256.p2p-spb.xyz:9338/web/graph_data/miner_hash_rates/last_hour', (err, response, miner_hash_rates_last_hour) => {
-  if(err)
-    console.log(err);
-    statistic.miner_hash_rates_last_hour = miner_hash_rates_last_hour
-    //console.log(response);
-    return miner_hash_rates_last_hour
-  });
-  request('http://sha256.p2p-spb.xyz:9338/web/graph_data/current_payouts/last_hour', (err, response, current_payouts_last_hour) => {
-  if(err)
-    console.log(err);
-    statistic.current_payouts_last_hour = current_payouts_last_hour
-    //console.log(response);
-    return current_payouts_last_hour
-  });
-  //resolve(rate,difficulty,users,user_stales)
-});
-promise.then(function(statistic) {
-  res.json(statistic)
-});
-}
 exports.list = function (req,res) {
-let   result = DB.listAlgoritm
-  function call(coin) {
-    request('http://p2p-ekb.xyz:'+coin.port+'/rate', (err, response, rate) => {
-    // if(err)
-    //   console.log(err,{port:coin.port});
-      coin.rate = rate
-      request('http://sha256.p2p-spb.xyz:'+coin.port+'/users', (err, response, users) => {
-      // if(err)
-      //   // console.log(err,{port:coin.port});
-        coin.users = users -1
-        request('http://sha256.p2p-spb.xyz:'+coin.port+'/fee', (err, response, fee) => {
-        // if(err)
-        //   // console.log(err,{port:coin.port});
-          coin.fee = fee
-          request('http://sha256.p2p-spb.xyz:'+coin.port+'/global_stats', (err, response, global_stats) => {
-          // if(err)
-          //   // console.log(err,{port:coin.port});
-            coin.global_stats = global_stats
-          });
-        });
-      });
+  let   coin = DB.listAlgoritm
+  let requests = coin.map(function (url) {
+      return new Promise((resolve, reject) => {
+            request('http://p2p-ekb.xyz:'+url.port+'/rate', (err, response, rate) => {
+              // if(err)
+              //   console.log(err,{port:coin.port});
+                //coin.rate = rate
+                request('http://p2p-ekb.xyz:'+url.port+'/users', (err, response, users) => {
+                // if(err)
+                //   // console.log(err,{port:coin.port});
+                  //coin.users = users.length -1
+                  request('http://p2p-ekb.xyz:'+url.port+'/fee', (err, response, fee) => {
+                  // if(err)
+                  //   // console.log(err,{port:coin.port});
+                    //coin.fee = fee
+                    request('http://p2p-ekb.xyz:'+url.port+'/global_stats', (err, response, global_stats) => {
+                    // if(err)
+                    //   // console.log(err,{port:coin.port});
+                      let result = {
+                        name:url.name,
+                        fullName:url.fullName,
+                        algoritm:url.algoritm,
+                        rate: rate,
+                        users:users,
+                        fee:fee,
+                        global_stats: global_stats
+                      }
+                      resolve(result)
+                    });
+                  });
+                });
+              });
+      })
 
     });
 
-  }
-  for (let  i = 0; i < result.length; i++) {
-    call(result[i])
-  }
-  var data = []
-  var buffer = []
-  var j = 0
-  var k = 0
-  var property = 'algoritm'
-  // buffer[j].algoritm = result[j].algoritm
-  for (var i = 0; i < result.length; i++) {
-    console.log(i);
-    buffer[k] = result[i]
-    if(result[i].global_stats !==undefined){
-      result[i].global_stats= JSON.parse(result[i].global_stats)
-    }
-    k++
-    if(result[i+1] === undefined   ||  result[i].algoritm !== result[i+1].algoritm){
-        // data[j].algoritm = result[i].algoritm
-        // data[j].coins = buffer
-        data[j] = {algoritm:result[i].algoritm,coins:buffer}
-        j++;
-        k = 0
-        buffer = []
-        //data[j].algoritm = result[i+1].algoritm
-    }
-  }
-//  res.json(data)
-  res.render('blank',{data})
+  Promise.all(requests)
+    .then(function (result) {
+      let data = []
+        let buffer = []
+        let j = 0
+        let k = 0
+        let property = 'algoritm'
+        // buffer[j].algoritm = result[j].algoritm
+        for (let i = 0; i < result.length; i++) {
+          //console.log(i);
+          buffer[k] = result[i]
+          // if(result[i].global_stats !=undefined){
+            result[i].global_stats = JSON.parse(result[i].global_stats)
+          // }
+          k++
+          if(result[i+1] === undefined   ||  result[i].algoritm !== result[i+1].algoritm){
+              // data[j].algoritm = result[i].algoritm
+              // data[j].coins = buffer
+              data[j] = {algoritm:result[i].algoritm,coins:buffer}
+              j++;
+              k = 0
+              buffer = []
+              //data[j].algoritm = result[i+1].algoritm
+          }
+        }
+      //res.json(data)
+        res.render('blank',{data})
+    });
 }
