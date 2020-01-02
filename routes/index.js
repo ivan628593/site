c943c9489ad8bc59d537e6296ddce4e3b2ee8c00
ivan628593 <ivanerasov96@gmail.com>
2020-01-02 21:49:26 +0300

    готовая главная страница(footer, конвертация чисел)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var Handlebars = require('hbs');
+var numeral = require('numeral');
+
+
 var router = express.Router();
 const controllerRequest = require('../controllers/request')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('blank');
+});
+
+numeral.register('locale', 'fr', {
+    delimiters: {
+        thousands: ' ',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'M',
+        billion: 'G',
+        trillion: 'T'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '€'
+    }
+});
+
+// switch between locales
+numeral.locale('fr');
+
+Handlebars.registerHelper('num',function(a){
+  return new Handlebars.SafeString(numeral(a).format("0 a").toUpperCase()+"h/s");//   numeral(a).format('00.0 a');
 });
 Handlebars.registerHelper('if_eq', function(a, b, opts) {
     if (a == b) {
@@ -13,6 +42,7 @@ Handlebars.registerHelper('if_eq', function(a, b, opts) {
         return opts.inverse(this);
     }
 });
+Handlebars
 
 router.get('/list',controllerRequest.list)
 module.exports = router;
